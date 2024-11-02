4109c2485483e459a9303a0eeb282f258c961682
Abhiram Pai <abhirampai1999@gmail.com>
2024-11-02 20:38:57 +0530

    Passing stage 'Handle concurrent clients'

--- app/server.rb
+++ app/server.rb
@@ -1,20 +1,40 @@
 require "socket"
 
-class YourRedisServer
+class RedisServer
+  attr_reader :server, :clients
   def initialize(port)
-    @port = port
+    @server = TCPServer.new(port)
+    @clients = []
   end
 
-  def start
-    server = TCPServer.new(@port)
-    client = server.accept
+  def listen
+    loop do
+      accept_incoming_connections
+    end
+  end
+
+  private
+
+  def accept_incoming_connections
+    begin
+      fds_to_watch = [@server, *@clients]
+      ready_to_read, _, _ = IO.select(fds_to_watch)
+      ready_to_read.each do |client|
+        if client == @server
+          @clients << @server.accept
+        else
+          handle_client(client)
+        end
+      end
+    rescue IO::WaitReadable, Errno::EINTR
+    end
+  end
+
+  def handle_client(client)
+    client.readpartial(1024)
+    client.write("+PONG\r\n")
+  rescue EOFError
   end
 end
 
-redis_cli = YourRedisServer.new(6379).start
-
-while line = redis_cli.gets
-  if(line.chomp.include?("PING"))
-    redis_cli.puts "+PONG\r\n"
-  end
-end
+redis_cli = RedisServer.new(6379).listen
